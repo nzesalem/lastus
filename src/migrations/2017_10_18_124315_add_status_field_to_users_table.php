c3307f8e180b688dbf3945f041c5a0d902333f40
Salem Nzeukwu <sjimfender@gmail.com>
2018-04-26 02:44:48 +0100

    Upgrade dependencies & Minor Change in migration

--- src/migrations/2017_10_18_124315_add_status_field_to_users_table.php
+++ src/migrations/2017_10_18_124315_add_status_field_to_users_table.php
@@ -14,14 +14,9 @@ class AddStatusFieldToUsersTable extends Migration
     public function up()
     {
         // If the users table does not already have the status column, we add it
-        // if it already has, we don't want to change it. we just set a flag
-        // which can be useful during rollback
         if (! Schema::hasColumn('users', 'status')) {
             Schema::table('users', function (Blueprint $table) {
                 $table->tinyInteger('status')->default(0);
-            });
-        } else {
-            Schema::table('users', function (Blueprint $table) {
                 $table->tinyInteger('lastus')->nullable()->default(1);
             });
         }
@@ -37,11 +32,12 @@ class AddStatusFieldToUsersTable extends Migration
         if (Schema::hasColumn('users', 'lastus')) {
             Schema::table('users', function (Blueprint $table) {
                 $table->dropColumn('lastus');
-            });
-        } elseif (Schema::hasColumn('users', 'status')) {
-            // if there's no lastus column, we assume we added the status column
-            Schema::table('users', function (Blueprint $table) {
-                $table->dropColumn('status');
+
+                if (Schema::hasColumn('users', 'status')) {
+                    Schema::table('users', function (Blueprint $table) {
+                        $table->dropColumn('status');
+                    });
+                }
             });
         }
     }
